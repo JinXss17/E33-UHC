a3be2fcd52f852d792428a9a26f0b147df844539
matgique <mattrous77320@gmail.com>
2025-07-23 11:50:07 +0200

    Création Gustave

---
+++ 1220320/src/main/java/fr/jinxss/e33/e33system/roles/Gustave.java
@@ -0,0 +1,26 @@
+package fr.jinxss.e33.e33system.roles;
+
+import java.util.UUID;
+
+import fr.jinxss.e33.Roles;
+
+public class Gustave extends Roles {
+
+	public Gustave(UUID uuid, String name) {
+		super(uuid, name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onAssign() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onPowerUse() {
+		// TODO Auto-generated method stub
+
+	}
+
+}

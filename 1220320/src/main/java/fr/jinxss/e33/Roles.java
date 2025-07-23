ea46bc2d777e0d5cec1bdb2b8186fff5fb571f26
matgique <mattrous77320@gmail.com>
2025-07-23 11:25:35 +0200

    Création du systême ROLES

---
+++ 1220320/src/main/java/fr/jinxss/e33/Roles.java
@@ -0,0 +1,58 @@
+package fr.jinxss.e33;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public abstract class Roles {
+
+    protected UUID uuid;
+    protected String name;
+    protected int speed;
+    protected int force;
+    protected int resi;
+
+    // Liste des objets associés au rôle (peut être vide, 1 ou plusieurs)
+    protected List<ItemStack> roleItems = new ArrayList<>();
+
+
+    public Roles(UUID uuid, String name) {
+        this.uuid = uuid;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Player getPlayer() {
+        return Bukkit.getPlayer(uuid);
+    }
+
+    public List<ItemStack> getRoleItems() {
+        return roleItems;
+    }
+
+    // Méthode appelée quand le rôle est attribué
+    public abstract void onAssign();
+
+    // Méthode appelée quand le joueur utilise son ou ses pouvoir(s)
+    public abstract void onPowerUse();
+    
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int getForce() {
+        return force;
+    }
+
+    public int getResi() {
+        return resi;
+    }
+
+}

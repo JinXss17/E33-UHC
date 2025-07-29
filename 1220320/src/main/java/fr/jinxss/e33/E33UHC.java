afc8e29ca670cda24595157991667c055e77d901
matgique <mattrous77320@gmail.com>
2025-07-29 16:11:21 +0200

    Ajout de 3 mob custom (sans le loot custom)

--- 1220320/src/main/java/fr/jinxss/e33/E33UHC.java
+++ 1220320/src/main/java/fr/jinxss/e33/E33UHC.java
@@ -2,11 +2,15 @@ package fr.jinxss.e33;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import fr.jinxss.e33.mobsystem.MobCustomSpawner;
+
 public class E33UHC extends JavaPlugin {
 
 	
  	@Override
     public void onEnable() {
+ 		//getServer().getPluginManager().registerEvents(new MobSpawnListener(), this);
+ 		new MobCustomSpawner(this).runTaskTimer(this, 20 * 10L, 20 * 5L); // 5 étant le nombre de secondes entre chaque spawn
         getLogger().info("MonPlugin est activé !");
     }
 
